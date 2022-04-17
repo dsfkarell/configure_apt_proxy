7899f4a1438c6ce34344e9197c000cc00d478305
Denis S. Francia Karell <dsfkarell@gmail.com>
2022-04-17 01:43:45 -0400

    Corrected text messages

--- configure_apt_proxy.sh
+++ configure_apt_proxy.sh
@@ -4,7 +4,7 @@ proxy_port=$(zenity --entry --text="Enter the proxy port")
 if [ $proxy_host ] && [ $proxy_port ]; then 
 	echo "Acquire::http::Proxy \"http://$proxy_host:$proxy_port/\";
 	Acquire::https::Proxy \"http://$proxy_host:$proxy_port/\";" > "/etc/apt/apt.conf.d/proxy.conf";
-	zenity --info --text="Proxy setted correctly";
+	zenity --info --text="Proxy configured correctly";
 else
-	zenity --error --text="The values provided are incorrectly";
+	zenity --error --text="The values provided are incorrect";
 fi
