058973a3836d65950a4c650264e453d2fd3b672d
Denis S. Francia Karell <dsfkarell@gmail.com>
2022-04-16 23:42:47 -0400

    Added script

---
+++ configure_apt_proxy.sh
@@ -0,0 +1,10 @@
+proxy_host=$(zenity --entry --text="Enter the proxy host")
+proxy_port=$(zenity --entry --text="Enter the proxy port")
+
+if [ $proxy_host ] && [ $proxy_port ]; then 
+	echo "Acquire::http::Proxy \"http://$proxy_host:$proxy_port/\";
+	Acquire::https::Proxy \"http://$proxy_host:$proxy_port/\";" > "/etc/apt/apt.conf.d/proxy.conf";
+	zenity --info --text="Proxy setted correctly";
+else
+	zenity --error --text="The values provided are incorrectly";
+fi
